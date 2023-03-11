fa046ddb2ea7d456994a5d22d60e92865f190f24
Niklas Mårdby <niklas.mardby@gmail.com>
2023-03-11 10:26:47 +0100

    Add first design and map generation

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 "use strict";
 
+const World = {
+  width: 6,
+  height: 6,
+};
+
+const Hero = {
+  x: 2,
+  y: 2,
+};
+
+// &#x1F920;
+
+const mainElement = document.querySelector("main");
+
+for (let i = 0; i < World.height; i++) {
+  let r = document.createElement("div");
+  r.className = "row";
+  for (let j = 0; j < World.width; j++) {
+    let c = document.createElement("div");
+    c.className = "cell";
+    r.append(c);
+  }
+  mainElement.append(r);
+}
